d2981a80865bb58330229bc672d54632912d4610
jiannlee22 <947807614@qq.com>
2025-12-26 14:32:38 +0800

    add baowm.h

---
+++ baowm.h
@@ -0,0 +1,14 @@
+#ifndef BAOWM_H
+#define BAOWM_H
+
+typedef struct baowm_server baowm_server;
+typedef struct baowm_output baowm_output;
+typedef struct baowm_toplevel baowm_toplevel;
+typedef struct baowm_popup baowm_popup;
+typedef struct baowm_keyboard baowm_keyboard;
+
+static void quit_handler(struct baowm_server *server);
+static void cycle_handler(struct baowm_server *server);
+static void menu_handler(struct baowm_server *server);
+static void menu_handler(struct baowm_server *server);
+#endif
--- config.h
+++ config.h
@@ -1,15 +1,17 @@
-/* See LICENSE file for copyright and license details. */
+#include "baowm.h"
+#include <xkbcommon/xkbcommon-keysyms.h>
+#include <xkbcommon/xkbcommon.h>
 
 #define MODKEY WLR_MODIFIER_ALT
 #define TERMINAL "kitty"
 #define MENU "wofi --show run"
 
 static const struct keycode_map {
-	xkb_keysym_t keysym;
-	void (*handler)(struct baowm_server *server);
+  xkb_keysym_t keysym;
+  void (*handler)(struct baowm_server *server);
 } keycodes[] = {
-	{ XKB_KEY_Escape, quit_handler },
-	{ XKB_KEY_F1, cycle_handler },
-	{ XKB_KEY_p, menu_handler },
-	{ XKB_KEY_P, menu_handler },
+    {XKB_KEY_Escape, quit_handler},
+    {XKB_KEY_F1, cycle_handler},
+    {XKB_KEY_p, menu_handler},
+    {XKB_KEY_Return, menu_handler},
 };
